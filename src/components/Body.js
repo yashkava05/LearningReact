eb92bb208db1b7856887779c024c37422f614c5d
yashkava05 <110620608+yashkava05@users.noreply.github.com>
2024-04-11 11:44:05 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,7 +19,7 @@ const Body = () => {
     );
     const json = await data.json();
 
-    console.log(json);
+    //console.log(json);
     setListOfRestaurants(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,15 +1,37 @@
-const RestaurantMenu = () => {
-    return(
-        <div className="menu">
-            <h1>Name of the restaurant</h1>
-            <h2>Menu</h2>
-            <ul>
-                <li>Sandwich</li>
-                <li>Pav bhaji</li>
-                <li>Dosa</li>
-            </ul>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
-export default RestaurantMenu
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=28405&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER")
+    const json = await data.json();
+
+    console.log(json);
+
+    setResInfo(json.data);
+  };
+
+  return resInfo === null ? (
+    <Shimmer />
+  ) : (
+    <div className="menu">
+      <h1>
+        
+      </h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Sandwich</li>
+        <li>Pav bhaji</li>
+        <li>Dosa</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
